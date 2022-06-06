777faeb93c769880bd801222007fde1e4a75634d
aliceauko <alice.auko@student.moringaschool.com>
2022-06-07 02:48:52 +0300

    commit tree 65d5cbc4d7c0cc9f236109ba1b89129a82ab0411

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1= 31;
+let num2= 2;
+let multiply = num1* num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = 24 % 5;
+
+let max  = Math.max(4, 8, 12, 16, 20)
+
+
